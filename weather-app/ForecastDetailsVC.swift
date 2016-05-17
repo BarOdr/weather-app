aebb3b6ffdd5d05b0ef336f5451b677efa1320e4
Bartosz Odrzywołek <Bart@Bartoszs-MacBook-Pro.local>
2016-05-17 20:49:08 +0200

    Improve main vc, configure details vc

---
+++ weather-app/ForecastDetailsVC.swift
@@ -0,0 +1,20 @@
+//
+//  ForecastDetailsVC.swift
+//  weather-app
+//
+//  Created by Bartosz Odrzywołek on 17/05/16.
+//  Copyright © 2016 Bartosz. All rights reserved.
+//
+
+import UIKit
+
+class ForecastDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
