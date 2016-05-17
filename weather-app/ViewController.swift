ec88de862efddf9e0c3ed3914dcebe3ad4063441
Bartosz Odrzywolek <bartosz.odrzywolek@gmail.com>
2016-05-17 12:50:35 +0200

    Initial Commit

---
+++ weather-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather-app
+//
+//  Created by Bartosz Odrzywołek on 17/05/16.
+//  Copyright © 2016 Bartosz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
