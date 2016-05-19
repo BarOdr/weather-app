a28583669347a063220cc467eac888cc4079d951
Bartosz Odrzywołek <Bart@Bartoszs-MacBook-Pro.local>
2016-05-19 14:16:43 +0200

    Connect outlets for mainVC

--- weather-app/ViewController.swift
+++ weather-app/ViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherDescImg: UIImageView!
+    @IBOutlet weak var cityLbl: UILabel!
+    @IBOutlet weak var dayLbl: UILabel!
+    @IBOutlet weak var tempLbl: UILabel!
+    @IBOutlet weak var descLbl: UILabel!
+    @IBOutlet weak var sunriseLbl: UILabel!
+    @IBOutlet weak var sunsetLbl: UILabel!
+    @IBOutlet weak var cloudinessLbl: UILabel!
+    @IBOutlet weak var windLbl: UILabel!
+    @IBOutlet weak var humidityLbl: UILabel!
+    
+    //Outlets for the bottom bar
+    
+    @IBOutlet weak var todayDescImg: UIImageView!
+    @IBOutlet weak var todayTempLbl: UILabel!
+    
+    @IBOutlet weak var in1DayWeekdayLbl: UILabel!
+    @IBOutlet weak var in1DayDescImg: UIImageView!
+    @IBOutlet weak var in1DayTempLbl: UILabel!
+    
+    @IBOutlet weak var in2DaysWeekdayLbl: UILabel!
+    @IBOutlet weak var in2DaysDescImg: UIImageView!
+    @IBOutlet weak var in2DaysTempLbl: UILabel!
+    
+    @IBOutlet weak var in3DaysWeekdayLbl: UILabel!
+    @IBOutlet weak var in3DaysDescImg: UIImageView!
+    @IBOutlet weak var in3DaysTempLbl: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
